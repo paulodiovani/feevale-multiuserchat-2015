dfcf48f02a10c55d24d0cc7937630790e8958db3
Paulo Diovani Gonçalves <paulo@diovani.com>
2015-10-15 20:17:41 -0300

    Add professor client classes

---
+++ src/ChatClient.java
@@ -0,0 +1,86 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import javax.swing.JTextArea;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gabriel
+ */
+public class ChatClient implements Runnable{
+    String host;
+    int porta;
+    Thread thCliente;
+    Socket s;
+    BufferedReader in;
+    PrintWriter out;
+    JTextArea txtSaida;
+
+    public void setTxtSaida(JTextArea txtSaida) {
+        this.txtSaida = txtSaida;
+    }
+    
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setPorta(int porta) {
+        this.porta = porta;
+    }
+
+    public int getPorta() {
+        return porta;
+    }
+    
+    public void configurarCliente(){
+        try {
+            s = new Socket(host, porta);
+            in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            out = new PrintWriter(s.getOutputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    public void iniciar(){
+        thCliente = new Thread(this);
+        thCliente.start();
+    }
+    
+    public void receberMensagens(){
+        try {
+            String msg;
+            while((msg = in.readLine()) != null){
+                txtSaida.append(msg);
+                txtSaida.append("\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void enviarMensagem(String msg){
+        out.println(msg);
+        out.flush();
+        System.out.println(msg);
+    }
+
+    @Override
+    public void run() {
+        configurarCliente();
+        receberMensagens();
+    }
+}
